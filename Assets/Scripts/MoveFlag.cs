0ece368c31682a8960d8127090727c1b7ab90add
JakobDutch <boriskusters@gmail.com>
2022-09-22 18:59:22 +0200

    sfx fix for pole

--- Assets/Scripts/MoveFlag.cs
+++ Assets/Scripts/MoveFlag.cs
@@ -12,7 +12,7 @@ public class MoveFlag : MonoBehaviour
     LayerMask mask;
 
     [SerializeField]
-    SoundManager sounds;
+    AudioSource sfx;
     void Start()
     {
         this.transform.Rotate(new Vector3(Random.Range(-10.0f, 10.0f), Random.Range(-180.0f, 180.0f), Random.Range(-10.0f, 10.0f)));
@@ -41,7 +41,7 @@ public class MoveFlag : MonoBehaviour
     bool FallCheck () {
         if (Physics.Raycast(gameObject.transform.position, Vector3.down, 0.7f, mask)) {
             Debug.Log("RayCast HIT!");
-            sounds.sfxClips[0].Play();
+            sfx.Play();
             Destroy(this);
             moveDown *= 0;
             return true;

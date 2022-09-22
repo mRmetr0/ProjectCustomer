6523930ec6fda92167043f7583e576072a572bc2
mRmetr0 <99254814+mRmetr0@users.noreply.github.com>
2022-09-22 13:52:15 +0200

    Revert "Merge pull request #12 from mRmetr0/itteration2"
    This reverts commit 37e784254c0bc55ebf9df22cca13ae837fb1e4d6, reversing
    changes made to 2d25c5c601cf675f85dc346502063cca205e718a.

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -6,6 +6,7 @@ using UnityEngine.SceneManagement;
 public class GameManager : MonoBehaviour
 {
     public static GameManager instance;
+
     private int difusedMines, maxMines, flagsPlaced = 0;
     public int difused {
         get{return difusedMines;}
--- Assets/Scripts/MineScript.cs
+++ Assets/Scripts/MineScript.cs
@@ -10,6 +10,8 @@ public class MineScript : MonoBehaviour
     private float difuseDistance;
     [SerializeField][Tooltip("If player places flag in this radius, the mine goes off.")]
     private float deathDistance;
+    [SerializeField]
+    private float minDistance;
     
 
     private Vector3 defusePos;
@@ -32,7 +34,7 @@ public class MineScript : MonoBehaviour
     }
     void FlagableMine() {
         if (!handled) {
-            flatDistance = new Vector2( player.transform.position.x - transform.position.x,  player.transform.position.z - transform.position.z);
+            flatDistance = new Vector3( player.transform.position.x - transform.position.x, 0,  player.transform.position.z - transform.position.z);
             if (flatDistance.magnitude <= deathDistance) {      //Puts flag on mine and detonates;
                 GameManager.instance.GoToScene("EndScene");
             } else if (flatDistance.magnitude <= difuseDistance) {  //Close enough to diffuse the bomb:
